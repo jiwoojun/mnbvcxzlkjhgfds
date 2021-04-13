d9ea2565efab5580471b53af6b3d2d4262e3e2ae
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2021-04-13 19:05:30 +1200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
+namespace SpriteKind {
+    export const item = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (upsider.isHittingTile(CollisionDirection.Top)) {
+        upsider.vy += 80
+    }
+    if (downsider.isHittingTile(CollisionDirection.Bottom)) {
+        downsider.vy += -80
+    }
+})
+scene.onOverlapTile(SpriteKind.item, assets.tile`myTile1`, function (sprite, location) {
+    game.over(true)
+})
+let downsider: Sprite = null
+let upsider: Sprite = null
 tiles.loadMap(tiles.createMap(tilemap`level1`))
-let upsider = sprites.create(img`
+upsider = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -17,7 +33,7 @@ let upsider = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-let downsider = sprites.create(img`
+downsider = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -37,5 +53,40 @@ let downsider = sprites.create(img`
     `, SpriteKind.Player)
 upsider.ay = -200
 downsider.ay = 200
-controller.moveSprite(upsider)
-controller.moveSprite(downsider)
+controller.moveSprite(upsider, 100, 0)
+controller.moveSprite(downsider, 100, 0)
+let downkey1 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . 5 5 5 . . . . . . . . . . . 
+    . . 5 . 5 5 5 5 5 5 5 5 . . . . 
+    . . 5 . 5 5 5 . . 5 . 5 . . . . 
+    . . 5 5 5 . 5 . . . . 5 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.item)
+downkey1.ay = 200
+tiles.coverAllTiles(assets.tile`myTile`, assets.tile`myTile0`)
+tiles.coverAllTiles(assets.tile`myTile3`, assets.tile`myTile0`)
+tiles.coverAllTiles(assets.tile`myTile4`, assets.tile`myTile0`)
+tiles.placeOnRandomTile(downkey1, assets.tile`myTile`)
+info.setScore(0)
+let keyspeed = 10
+tiles.placeOnRandomTile(upsider, assets.tile`myTile3`)
+tiles.placeOnRandomTile(downsider, assets.tile`myTile4`)
+game.onUpdate(function () {
+    if (spriteutils.distanceBetween(upsider, downkey1) < 10) {
+        spriteutils.setVelocityAtAngle(downkey1, spriteutils.angleFrom(upsider, downkey1), keyspeed)
+    }
+    if (spriteutils.distanceBetween(downsider, downkey1) < 10) {
+        spriteutils.setVelocityAtAngle(downkey1, spriteutils.angleFrom(downsider, downkey1), keyspeed)
+    }
+})
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,20 +2,32 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`0a0008000101010101010101010100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002020202020202020202`, img`
+            case "level1":return tiles.createTilemap(hex`0a0008000101010101010101010107050505040505050508050505030305050505050505050505050505050505050505050505050505050505050505030305050905050505050505050602020202020202020202`, img`
 2 2 2 2 2 2 2 2 2 2 
 . . . . . . . . . . 
+. . . 2 2 . . . . . 
 . . . . . . . . . . 
 . . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
+. . . . . . 2 2 . . 
 . . . . . . . . . . 
 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.floorDark0], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.floorDark0,sprites.dungeon.floorMixed,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6], TileScale.Sixteen);
         }
         return null;
     })
@@ -23,6 +35,18 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
         }
         return null;
     })

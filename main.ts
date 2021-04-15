c5b9c39105ec5c78619c4b82065d15f1528e943f
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2021-04-15 19:25:48 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 namespace SpriteKind {
     export const item = SpriteKind.create()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (upsider.isHittingTile(CollisionDirection.Top)) {
+        upsider.vy += 100
+    }
+    if (downsider.isHittingTile(CollisionDirection.Bottom)) {
+        downsider.vy += -100
+    }
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
     game.over(false)
 })
@@ -14,11 +22,86 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (upsider.isHittingTile(CollisionDirection.Top)) {
-        upsider.vy += 100
-    }
-    if (downsider.isHittingTile(CollisionDirection.Bottom)) {
-        downsider.vy += -100
+    if (gravity == true) {
+        downsider.ay = -200
+        downsider.setImage(img`
+            . . . . . . . a . a a . . . . . 
+            . . . . . . . d . d . . . . . . 
+            . . . . . . . c . c . . . . . . 
+            . . . . . . . d . d . . . . . . 
+            . . . . . . . 2 8 2 . . . . . . 
+            . . . . . 2 . d d d . 2 . . . . 
+            . . . . . d . 2 a 2 . d . . . . 
+            . . . . . d . 2 a 2 . d . . . . 
+            . . . . . . d 2 2 2 d . . . . . 
+            . . . . . . . . d . . . . . . . 
+            . . . . . . . d d d . . . . . . 
+            . . . . . . 5 5 5 5 5 . . . . . 
+            . . . . . . . 5 4 5 . . . . . . 
+            . . . . . . . . 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        upsider.ay = 200
+        upsider.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 5 . 5 . 5 5 . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . 8 9 6 9 6 8 . . . . . 
+            . . . . . 8 6 9 6 6 8 . . . . . 
+            . . . . . . d d d d . . . . . . 
+            . . . . . . . d d . . . . . . . 
+            . . . . d b 4 4 4 4 b d . . . . 
+            . . . . d b 4 4 4 4 b d . . . . 
+            . . . . d . 4 4 4 4 . d . . . . 
+            . . . . 3 . e e e e . 3 . . . . 
+            . . . . . . e e . e . . . . . . 
+            . . . . . . 8 8 . 8 . . . . . . 
+            . . . . . e e e . e e . . . . . 
+            `)
+        gravity = false
+    } else {
+        downsider.ay = 200
+        downsider.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 5 . . . . . . . . 
+            . . . . . . 5 4 5 . . . . . . . 
+            . . . . . 5 5 5 5 5 . . . . . . 
+            . . . . . . d d d . . . . . . . 
+            . . . . . . . d . . . . . . . . 
+            . . . . . d 2 2 2 d . . . . . . 
+            . . . . d . 2 a 2 . d . . . . . 
+            . . . . d . 2 a 2 . d . . . . . 
+            . . . . 2 . d d d . 2 . . . . . 
+            . . . . . . 2 8 2 . . . . . . . 
+            . . . . . . d . d . . . . . . . 
+            . . . . . . c . c . . . . . . . 
+            . . . . . . d . d . . . . . . . 
+            . . . . . a a . a . . . . . . . 
+            `)
+        upsider.ay = -200
+        upsider.setImage(img`
+            . . . . . e e . e e e . . . . . 
+            . . . . . . 8 . 8 8 . . . . . . 
+            . . . . . . e . e e . . . . . . 
+            . . . . 3 . e e e e . 3 . . . . 
+            . . . . d . 4 4 4 4 . d . . . . 
+            . . . . d b 4 4 4 4 b d . . . . 
+            . . . . d b 4 4 4 4 b d . . . . 
+            . . . . . . . d d . . . . . . . 
+            . . . . . . d d d d . . . . . . 
+            . . . . . 8 6 6 9 6 8 . . . . . 
+            . . . . . 8 6 9 6 9 8 . . . . . 
+            . . . . . . 5 5 5 5 . . . . . . 
+            . . . . . 5 5 . 5 . 5 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        gravity = true
     }
 })
 scene.onOverlapTile(SpriteKind.item, assets.tile`myTile1`, function (sprite, location) {
@@ -27,6 +110,7 @@ scene.onOverlapTile(SpriteKind.item, assets.tile`myTile1`, function (sprite, loc
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
     game.over(false)
 })
+let gravity = false
 let camera = false
 let downsider: Sprite = null
 let upsider: Sprite = null
@@ -98,6 +182,7 @@ let keyspeed = 10
 tiles.placeOnRandomTile(upsider, assets.tile`myTile3`)
 tiles.placeOnRandomTile(downsider, assets.tile`myTile4`)
 camera = true
+gravity = true
 game.onUpdate(function () {
     if (spriteutils.distanceBetween(upsider, downkey1) < 10) {
         spriteutils.setVelocityAtAngle(downkey1, spriteutils.angleFrom(upsider, downkey1), keyspeed)
